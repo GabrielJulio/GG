96fb8d9e8829b2051ccd1793c7f2573187054964
GabrielJulio <gabrieljuliobs@gmail.com>
2019-08-01 23:16:53 -0300

    Create Implemented, routes will be merged.

--- Vehicle Management/fleet/urls.py
+++ Vehicle Management/fleet/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('viagem/', views.listagem_viagem, name='listagem_viagem'),
     path('viagem/nova', views.nova_viagem, name='nova_viagem'),
     path('motorista/', views.listagem_motorista, name='listagem_motorista'),
+    path('motorista/novo', views.novo_motorista, name='novo_motorista'),
     path('veiculo/', views.listagem_veiculo, name='listagem_veiculo'),
+    path('veiculo/novo', views.novo_veiculo, name='novo_veiculo'),
 ]
--- Vehicle Management/fleet/views.py
+++ Vehicle Management/fleet/views.py
@@ -7,32 +7,53 @@ from .form import VeiculoForm, MotoristaForm, ViagemForm
 def home(request):
 	return render(request, 'index.html')
 
+
 def meu_logout(request):
     logout(request)
     return redirect('/')
 
+
 def index(request):
 	return render(request, 'fleet/index.html')
+
 
 def listagem_viagem(request):
 	data = {}
 	data['viagens'] = Viagem.objects.all()
 	return render(request, 'fleet/read/viagem.html', data)
 
+
 def listagem_motorista(request):
 	data = {}
 	data['motoristas'] = Motorista.objects.all()
 	return render(request, 'fleet/read/motorista.html', data)
+
 
 def listagem_veiculo(request):
 	data = {}
 	data['veiculos'] = Veiculo.objects.all()
 	return render(request, 'fleet/read/veiculo.html', {'data': data})
 
+
 def nova_viagem(request):
 	form = ViagemForm(request.POST or None)
-	link = 1
 	if form.is_valid():
 		form.save()
 		return redirect('/frota/viagem')
+	return render(request, 'fleet/create/form.html', {'form': form})
+
+
+def novo_veiculo(request):
+	form = VeiculoForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect('/frota/veiculo')
+	return render(request, 'fleet/create/form.html', {'form': form})
+
+
+def novo_motorista(request):
+	form = MotoristaForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect('/frota/motorista')
 	return render(request, 'fleet/create/form.html', {'form': form})
