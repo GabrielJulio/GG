a4f4d8a89c038c7fe910bdf68c6fe62afee1463c
GabrielJulio <gabriljuliobs@gmail.com>
2019-12-11 16:38:07 -0300

    Hotfix 1.2.10 - Alterações nas migrations
    - Alteração na migration 0004 e deleção das posteriores.

--- fleet/models.py
+++ fleet/models.py
@@ -13,9 +13,9 @@ class Veiculo(models.Model):
 	]
 	tipo_veiculo = models.CharField(max_length=5, choices=TIPO_VEICULO_ESCOLHAS, default=CARRO)
 	nome = models.CharField(max_length=50)
-	marca = models.CharField(max_length=40)
+	marca = models.CharField(max_length=40, null=True)
 	ano = models.IntegerField(validators=[MinValueValidator(0, None), MaxValueValidator(9999, None)])
-	placa = models.CharField(max_length=8)
+	placa = models.CharField(max_length=8, null=True)
 	observacoes = models.TextField(max_length=1500, blank=True)
 
 	def __str__(self):
@@ -24,7 +24,7 @@ class Veiculo(models.Model):
 
 class Motorista(models.Model):
 	nome = models.CharField(max_length=50)
-	cpf = models.CharField(max_length=14)
+	cpf = models.CharField(max_length=14, null=True)
 	data_nascimento = models.DateField("data de nascimento", null=True)
 	observacoes = models.TextField(max_length=1500, blank=True)
 
@@ -33,8 +33,8 @@ class Motorista(models.Model):
 
 
 class Viagem(models.Model):
-	veiculo = models.ForeignKey(Veiculo, on_delete=None)
-	motorista = models.ForeignKey(Motorista, on_delete=None)
+	veiculo = models.ForeignKey(Veiculo, on_delete=models.SET_NULL, null=True)
+	motorista = models.ForeignKey(Motorista, on_delete=models.SET_NULL, null=True)
 	data = models.DateField("data da viagem")
 	saida = models.TimeField("horario da saida")
 	retorno = models.TimeField("horario do retorno", null=True, blank=True)
